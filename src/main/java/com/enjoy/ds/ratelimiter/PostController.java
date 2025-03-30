073a1aa0cc3d09a0a0cd572b3bf596266df3d594
spartan-vutran <vu.tran@c0x12c.com>
2025-03-30 14:54:16 +0700

    feat: init the project

---
+++ src/main/java/com/enjoy/ds/ratelimiter/PostController.java
@@ -0,0 +1,13 @@
+package com.enjoy.ds.ratelimiter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PostController {
+    @GetMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot";
+    }
+}
---
+++ src/main/java/com/enjoy/ds/ratelimiter/RatelimiterApplication.java
@@ -0,0 +1,39 @@
+package com.enjoy.ds.ratelimiter;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+/**
+ * @SpringBootApplication is a convenience annotation that adds all of the following:
+ *
+ * @Configuration: Tags the class as a source of bean definitions for the application context.
+ *
+ * @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings. For example, if spring-webmvc is on the classpath, this annotation flags the application as a web application and activates key behaviors, such as setting up a DispatcherServlet.
+ *
+ * @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
+ */
+@SpringBootApplication
+public class RatelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatelimiterApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+}
